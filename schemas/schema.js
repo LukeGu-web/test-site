418cef10bbd678b23d6677e7bdd8ea462241f75e
LukeGu <luke.gu.work@gmail.com.au>
2022-10-18 11:44:51 +1100

    chore: add schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,11 +1,16 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import animal from './animal';
+import blockContent from './blockContent';
+import author from './author';
+import post from './post';
+import category from './category';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
-})
+  types: schemaTypes.concat([animal, blockContent, author, post, category]),
+});
